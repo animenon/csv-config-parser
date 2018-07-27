0ed2bbf7d489fe98f49ac00533bcf94dd19ea4c3
Anirudh <animenon@mail.com>
2018-07-27 11:38:30 +0530

    Sample Config file added
    basic functions

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
-#!/usr/bin/env python3
-contents=['# Specify configurations as follows -\n', '# header1, name1:val1, name2:val2\n', '# ...\n', '# each line is a config\n', '\n', 'test, column1 = type1, column2 = type2\n', 'test1, column1 = type11, column2 = type22\n', 'test3, column1 = type111, column2 = type222\n']
+class CsvConfigParser:
+    def __init__(self, filepath):
+        self.result_dict = {}  # result_dict to be returned
+        self.file_path = filepath
+        self.csv_config_reader()
 
-contents2=[x.strip() for x in contents if(x[0]!=";" and x[0]!="#")]  ## remove comments
-contents2 = filter(None,contents2)
+    def csv_config_reader(self):
+        """
+        Read the CSV config file and return a dictionary with configuration sections as keys
 
-dict={}
-for x in contents2:
-    lst=x.split(",")    ## lst=[incubating.test, column1 = type1, column2 = type2]
-    header=lst.pop(0)       ## header = incubating.test ;;;; lst=['column1 = type1', 'column2 = type2']
-    while(len(lst)>0):
-        l=lst[0]
-        print(header, l)
-        if("=" in l):
-            pair=l.split("=")
-            if(header not in dict):
-                dict[header]={}
-            dict[header][pair[0].strip()] = pair[1].strip()
-            key=pair[0].strip()
-            val=pair[1].strip()
-            print("dict[{}][{}] = {}".format(header,key,val))
-        lst.pop(0)
+        :return: dictionaries of section(s) which contain Configuration Key-Value pairs
+        """
+        print("CSV config parser started.")
+        with open(self.file_path) as f:
+            contents = f.readlines()
+            contents2 = [x.strip() for x in contents if (x[0] != ";" and x[0] != "#")]    # remove comments
+            contents2 = filter(None, contents2)                                           # remove empty rows
+            for x in contents2:
+                lst = x.split(",")
+                header = lst.pop(0)
+                while len(lst) > 0:
+                    l = lst[0]
+                    # print(header, l)
+                    if "=" in l:
+                        pair = l.split("=")
+                        if header not in self.result_dict:
+                            self.result_dict[header] = {}
+                        self.result_dict[header][pair[0].strip()] = pair[1].strip()
+                        key = pair[0].strip()
+                        val = pair[1].strip()
+                    lst.pop(0)
+        print(self.result_dict)
+        return 0
+
+    def get_config(self, section, key):
+        """
+        Provide section name to get key value pairs in section
+
+        :param section:
+        :param key:
+        :return:
+        """
+        return self.result_dict[section][key]
+
+    def sections(self):
+        """
+        Return all sections
+        :return:
+        """
+        return list(self.result_dict.keys())
+
+"""Test """
+if __name__ == '__main__':
+    parser = CsvConfigParser("sample_config.csv")
+    print(parser.get_config("test1", "column1"))
+    print(parser.sections())
